887fa0b23a19a80c51068f1793fda5f99de4220a
PortalHUN <giotomi03@gmail.com>
2023-04-24 10:37:33 +0100

    added script.sh to start with systemctl

---
+++ script.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+
+cd /home/portal/website
+export NODE_ENV=development
+npm run dev
